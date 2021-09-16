1107b69948cf66ff87548920090fbff2671ef609
UltimateHikari <andy@andystpad.localdomain>
2021-09-16 19:51:36 +0700

    thread-safe exit, printf->write

--- lab1/1.c
+++ lab1/1.c
@@ -4,16 +4,27 @@
 #define _GNU_SOURCE
 #include <errno.h>
 #include <string.h>
-#define TEXT "Lorem ipsum dolor sit amet, consectetur adipiscing elit\n"
+#include <unistd.h>
+#define TEXT "Lorem ipsum dolor sit amet"
 #define COUNT 10
 #define BUFLEN 128
 
 pid_t gettid(void);
 
 void * thread_body(void * param) {
+    char msg_buf[BUFLEN];
     pthread_t pid = pthread_self();
+    if(sprintf(msg_buf, "[%d]: %s _\n", pid, TEXT) < 0){
+        perror("Sprintf");
+        pthread_exit(NULL);
+    }
+    int len = strlen(msg_buf);
     for(int i = 0; i < COUNT; i++){
-        printf("[%d]: %d - %s", pid, i, TEXT);
+        msg_buf[len - 2] = '0' + i;
+        if(write(1, msg_buf, len) < 0){
+            perror("Write");
+            pthread_exit(NULL);
+        }
     }
 }
 
@@ -22,23 +33,35 @@ int main(int argc, char *argv[]) {
     int rc;
     char err_buf[BUFLEN];
 
+    if(argc < 2){
+        printf("specify lab number [1/2]\n");
+        pthread_exit(NULL); 
+    }
+    
+    printf("parent thread id: %d\n", pthread_self());
+
     if ((rc = pthread_create(&thread, NULL, thread_body, NULL)) < 0) {
         fprintf(stderr, "Error creating thread: %s\n", strerror_r(rc, err_buf, BUFLEN));
-        exit(1);
+        pthread_exit(NULL);
     }
 
-    thread_body(NULL);
+    if(atoi(argv[1]) == 0){
+        thread_body(NULL);
+    }
 
     void * retval;
     if ((rc = pthread_join(thread, &retval)) < 0) {
         fprintf(stderr, "Error joining thread: %s\n", strerror_r(rc, err_buf, BUFLEN));
-        exit(1);
+        pthread_exit(NULL);
     }
-
+    
+    if(atoi(argv[1]) == 1){
+        thread_body(NULL);
+    }
     if (retval == PTHREAD_CANCELED) {
         printf("Thread was cancelled\n");
     } else { 
         printf("Thread was joined normally\n");
     }
-    return (EXIT_SUCCESS);
+    pthread_exit(NULL);
 }
