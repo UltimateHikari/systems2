cb98f24bbb56426dbd36d7cca8fdaa8ca67b8a22
UltimateHikari <andy@andystpad.localdomain>
2021-10-22 17:31:56 +0700

    lab16-proc

--- lab16/16.c
+++ lab16/16.c
@@ -4,14 +4,16 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
-#include <unistd.h>
 #include <semaphore.h>
 #include <fcntl.h>
+#include <sys/wait.h>
+#include "verify.h"
 
 #define TEXT "Lorem ipsum dolor sit amet"
 #define COUNT 10
 #define BUFLEN 128
 #define NOT_SHARED 0
+#define FORK_SUCCESS 0
 #define SEM_PRT_NAME "/semParent"
 #define SEM_CLD_NAME "/semChild"
 
@@ -20,67 +22,55 @@ pthread_t parent_thread_id;
 sem_t * parent_sem, * child_sem;
 
 void free_resources(){
+    sem_close(parent_sem);
     sem_unlink(SEM_PRT_NAME);
+    sem_close(child_sem);
     sem_unlink(SEM_CLD_NAME);
 }
 
-char err_buf[BUFLEN];
-void verify(int rc, const char* action){
-    strerror_r(rc, err_buf, BUFLEN);
-    if(rc != 0){
-        fprintf(stderr, "Error %s: %s\n",
-         action, err_buf);
-        free_resources();
-        pthread_exit(NULL);
-    }
-}
-void verify_e(int rc, const char* action){
-    if(rc < 0){
-        perror(action);
-        free_resources();
-        pthread_exit(NULL);
-    }
-}
-
-void * thread_body(void * unused) {
+void * process_body(int pid) {
     char msg_buf[BUFLEN];
-    pthread_t pid = pthread_self();
-    sem_t * my_sem = (pid == parent_thread_id ? parent_sem : child_sem);
-    sem_t * other_sem = (pid == parent_thread_id ? child_sem : parent_sem);
+    sem_t * my_sem = (pid != 0 ? parent_sem : child_sem);
+    sem_t * other_sem = (pid != 0 ? child_sem : parent_sem);
 
-    verify_e(sprintf(msg_buf, "[%ld]: %s _\n", pid, TEXT), "sprintf");
+    verify_e(sprintf(msg_buf, "[%d]: %s _\n", pid, TEXT), "sprintf", free_resources);
     int len = strlen(msg_buf);
 
     for(int i = 0; i < COUNT; i++){
         msg_buf[len - 2] = '0' + i;
         sem_wait(my_sem);
-        verify_e(write(1, msg_buf, len), "write");
+        verify_e(write(1, msg_buf, len), "write", free_resources);
         sem_post(other_sem);
     }
     return NULL;
 }
 
 int main() {
-    pthread_t thread;
+    int wstatus, wres;
+    pid_t pid = fork();
+
+    if (pid < FORK_SUCCESS){
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
 
     parent_sem = sem_open(SEM_PRT_NAME, O_CREAT, 0600, 1);
-    child_sem = sem_open(SEM_CLD_NAME, O_CREAT, 0600, 1);
+    child_sem = sem_open(SEM_CLD_NAME, O_CREAT, 0600, 0);
 
-    parent_thread_id = pthread_self();   
-    printf("parent thread id: %ld\n", parent_thread_id);
+    process_body(pid);
 
-    verify(pthread_create(&thread, NULL, thread_body, NULL), "pcreate");
-    
-    thread_body(NULL);
-
-    void * retval;
-    verify(pthread_join(thread, &retval), "pjoin");
-    
-    if (retval == PTHREAD_CANCELED) {
-        printf("Thread was cancelled\n");
-    } else { 
-        printf("Thread was joined normally\n");
+    if(pid != 0){
+    verify(wait(&wstatus), "wait", free_resources); //waitpid(-1,&wstatus) -/-1/0/>
+ 
+    if(WIFEXITED(wstatus)){
+        printf("child's exit status is: %d\n",
+        WEXITSTATUS(wstatus));
+    }else if (WIFSIGNALED(wstatus)){
+        printf("signal is: %d\n",
+        WTERMSIG(wstatus));
     }
+    }
+
     free_resources();
     pthread_exit(NULL);
 }
